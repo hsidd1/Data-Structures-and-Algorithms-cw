f34f7f1981597c4e9b94e00e02f5c6d8ae827319
Hamza Siddiqui <120290526+hsidd1@users.noreply.github.com>
2023-01-21 15:31:15 -0500

    safety upload

--- 2si Lab1/HugeInteger.cpp
+++ 2si Lab1/HugeInteger.cpp
@@ -1,29 +1,44 @@
 #include "HugeInteger.h"
 
+
 HugeInteger::HugeInteger(const std::string& val) {
-	std::vector<int> number;
-	for (int i = 0; i < val.length(); i++) {
+
+/* creates a HugeInteger from the decimal string representation val.
+The string contains an optional minus sign at the beginning followed by one or more decimal digits.
+No other characters are allowed in the string. */
+
+	int start = 0; // standard start for interating 'val'
+	if (val.length() == 0) 
+		throw std::invalid_argument("Error: Empty string!");
+	if (val[0] == '-') {
+		sign = true; 
+		start = 1; // skip first character in iteration if its a '-'
+	}
+	for (int i = start; i < val.length(); i++) {
 		// add to vector if valid ascii character 
 		if (val[i] >= 48 && val[i] <= 57) {
 			number.push_back(val[i] - 48);
 		}
 		else {
-			throw std::invalid_argument("Error: received invalid ascii input");
+			throw std::invalid_argument("Error: Received invalid ASCII input");
 		}
 	}
 }
 
 HugeInteger::HugeInteger(int n) {
+
+/* creates a random HugeInteger of n digits, the first digit
+being different from 0; n must be larger or equal to 1.*/
+
 	if (n < 1) {
 		throw std::invalid_argument("Error: n must be larger than or equal to 1");
 	}
 	srand(time(NULL));
 	unsigned int digit = 0;
-	std::vector<int> number;
 	// add random digits based on input total digit amount
 	for (int i = 0; i < n; i++) {
 		digit = rand() % 10; // random num 0 - 9
-		// if starting with leading 0 change to random num 1 - 9
+		// ensure no leading 0's 
 		if (i == 0 && digit == 0) {
 			digit = rand() % 9 + 1;
 		}
@@ -31,8 +46,10 @@ HugeInteger::HugeInteger(int n) {
 	}
 }
 
-/* Returns a new HugeInteger representing the sum of this HugeInteger and h.*/
 HugeInteger HugeInteger::add(const HugeInteger& h) {
+
+/* Returns a new HugeInteger representing the sum of this HugeInteger and h.*/
+
 
 	return HugeInteger("");
 }
@@ -53,7 +70,16 @@ int HugeInteger::compareTo(const HugeInteger& h) {
 }
 
 std::string HugeInteger::toString() {
-	std::string output = "";
-	for (int i = 0; i < this->HugeInteger.length(); )
+
+/* Returns a string representing the sequence of digits
+corresponding to the decimal representation of this HugeInteger */
+
+	std::string output;
+	if (sign) {
+		output += "-";
+	}
+	for (int i = 0; i < this->number.size(); i++) {
+		output += std::to_string(this->number[i]);
+	}
 	return output;
 }
